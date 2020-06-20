a21d0f272716cc2c963bbae6dfcd84eb89fbb9bc
Jan Odvarko <jan@odvarko.cz>
2020-06-20 12:00:20 +0200

    removed vml

--- jscolor.js
+++ jscolor.js
@@ -1027,31 +1027,6 @@ var jsc = {
 	},
 
 
-	_vmlNS : 'jsc_vml_',
-	_vmlCSS : 'jsc_vml_css_',
-	_vmlReady : false,
-
-
-	initVML : function () {
-		if (!jsc._vmlReady) {
-			// init VML namespace
-			var doc = document;
-			if (!doc.namespaces[jsc._vmlNS]) {
-				doc.namespaces.add(jsc._vmlNS, 'urn:schemas-microsoft-com:vml');
-			}
-			if (!doc.styleSheets[jsc._vmlCSS]) {
-				var tags = ['shape', 'shapetype', 'group', 'background', 'path', 'formulas', 'handles', 'fill', 'stroke', 'shadow', 'textbox', 'textpath', 'imagedata', 'line', 'polyline', 'curve', 'rect', 'roundrect', 'oval', 'arc', 'image'];
-				var ss = doc.createStyleSheet();
-				ss.owningElement.id = jsc._vmlCSS;
-				for (var i = 0; i < tags.length; i += 1) {
-					ss.addRule(jsc._vmlNS + '\\:' + tags[i], 'behavior:url(#default#VML);');
-				}
-			}
-			jsc._vmlReady = true;
-		}
-	},
-
-
 	createPalette : function () {
 
 		var paletteObj = {
@@ -1059,114 +1034,44 @@ var jsc = {
 			draw: null
 		};
 
-		if (jsc.isCanvasSupported) {
-			// Canvas implementation for modern browsers
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
 
-			var canvas = document.createElement('canvas');
-			var ctx = canvas.getContext('2d');
+		var drawFunc = function (width, height, type) {
+			canvas.width = width;
+			canvas.height = height;
 
-			var drawFunc = function (width, height, type) {
-				canvas.width = width;
-				canvas.height = height;
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
+			var hGrad = ctx.createLinearGradient(0, 0, canvas.width, 0);
+			hGrad.addColorStop(0 / 6, '#F00');
+			hGrad.addColorStop(1 / 6, '#FF0');
+			hGrad.addColorStop(2 / 6, '#0F0');
+			hGrad.addColorStop(3 / 6, '#0FF');
+			hGrad.addColorStop(4 / 6, '#00F');
+			hGrad.addColorStop(5 / 6, '#F0F');
+			hGrad.addColorStop(6 / 6, '#F00');
 
-				var hGrad = ctx.createLinearGradient(0, 0, canvas.width, 0);
-				hGrad.addColorStop(0 / 6, '#F00');
-				hGrad.addColorStop(1 / 6, '#FF0');
-				hGrad.addColorStop(2 / 6, '#0F0');
-				hGrad.addColorStop(3 / 6, '#0FF');
-				hGrad.addColorStop(4 / 6, '#00F');
-				hGrad.addColorStop(5 / 6, '#F0F');
-				hGrad.addColorStop(6 / 6, '#F00');
+			ctx.fillStyle = hGrad;
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-				ctx.fillStyle = hGrad;
-				ctx.fillRect(0, 0, canvas.width, canvas.height);
+			var vGrad = ctx.createLinearGradient(0, 0, 0, canvas.height);
+			switch (type.toLowerCase()) {
+			case 's':
+				vGrad.addColorStop(0, 'rgba(255,255,255,0)');
+				vGrad.addColorStop(1, 'rgba(255,255,255,1)');
+				break;
+			case 'v':
+				vGrad.addColorStop(0, 'rgba(0,0,0,0)');
+				vGrad.addColorStop(1, 'rgba(0,0,0,1)');
+				break;
+			}
+			ctx.fillStyle = vGrad;
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+		};
 
-				var vGrad = ctx.createLinearGradient(0, 0, 0, canvas.height);
-				switch (type.toLowerCase()) {
-				case 's':
-					vGrad.addColorStop(0, 'rgba(255,255,255,0)');
-					vGrad.addColorStop(1, 'rgba(255,255,255,1)');
-					break;
-				case 'v':
-					vGrad.addColorStop(0, 'rgba(0,0,0,0)');
-					vGrad.addColorStop(1, 'rgba(0,0,0,1)');
-					break;
-				}
-				ctx.fillStyle = vGrad;
-				ctx.fillRect(0, 0, canvas.width, canvas.height);
-			};
-
-			paletteObj.elm = canvas;
-			paletteObj.draw = drawFunc;
-
-		} else {
-			// VML fallback for IE 7 and 8
-
-			jsc.initVML();
-
-			var vmlContainer = document.createElement('div');
-			vmlContainer.style.position = 'relative';
-			vmlContainer.style.overflow = 'hidden';
-
-			var hGrad = document.createElement(jsc._vmlNS + ':fill');
-			hGrad.type = 'gradient';
-			hGrad.method = 'linear';
-			hGrad.angle = '90';
-			hGrad.colors = '16.67% #F0F, 33.33% #00F, 50% #0FF, 66.67% #0F0, 83.33% #FF0'
-
-			var hRect = document.createElement(jsc._vmlNS + ':rect');
-			hRect.style.position = 'absolute';
-			hRect.style.left = -1 + 'px';
-			hRect.style.top = -1 + 'px';
-			hRect.stroked = false;
-			hRect.appendChild(hGrad);
-			vmlContainer.appendChild(hRect);
-
-			var vGrad = document.createElement(jsc._vmlNS + ':fill');
-			vGrad.type = 'gradient';
-			vGrad.method = 'linear';
-			vGrad.angle = '180';
-			vGrad.opacity = '0';
-
-			var vRect = document.createElement(jsc._vmlNS + ':rect');
-			vRect.style.position = 'absolute';
-			vRect.style.left = -1 + 'px';
-			vRect.style.top = -1 + 'px';
-			vRect.stroked = false;
-			vRect.appendChild(vGrad);
-			vmlContainer.appendChild(vRect);
-
-			var drawFunc = function (width, height, type) {
-				vmlContainer.style.width = width + 'px';
-				vmlContainer.style.height = height + 'px';
-
-				hRect.style.width =
-				vRect.style.width =
-					(width + 1) + 'px';
-				hRect.style.height =
-				vRect.style.height =
-					(height + 1) + 'px';
-
-				// Colors must be specified during every redraw, otherwise IE won't display
-				// a full gradient during a subsequential redraw
-				hGrad.color = '#F00';
-				hGrad.color2 = '#F00';
-
-				switch (type.toLowerCase()) {
-				case 's':
-					vGrad.color = vGrad.color2 = '#FFF';
-					break;
-				case 'v':
-					vGrad.color = vGrad.color2 = '#000';
-					break;
-				}
-			};
-			
-			paletteObj.elm = vmlContainer;
-			paletteObj.draw = drawFunc;
-		}
+		paletteObj.elm = canvas;
+		paletteObj.draw = drawFunc;
 
 		return paletteObj;
 	},
@@ -1179,65 +1084,25 @@ var jsc = {
 			draw: null
 		};
 
-		if (jsc.isCanvasSupported) {
-			// Canvas implementation for modern browsers
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
 
-			var canvas = document.createElement('canvas');
-			var ctx = canvas.getContext('2d');
+		var drawFunc = function (width, height, color1, color2) {
+			canvas.width = width;
+			canvas.height = height;
 
-			var drawFunc = function (width, height, color1, color2) {
-				canvas.width = width;
-				canvas.height = height;
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
+			var grad = ctx.createLinearGradient(0, 0, 0, canvas.height);
+			grad.addColorStop(0, color1);
+			grad.addColorStop(1, color2);
 
-				var grad = ctx.createLinearGradient(0, 0, 0, canvas.height);
-				grad.addColorStop(0, color1);
-				grad.addColorStop(1, color2);
+			ctx.fillStyle = grad;
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+		};
 
-				ctx.fillStyle = grad;
-				ctx.fillRect(0, 0, canvas.width, canvas.height);
-			};
-
-			sliderObj.elm = canvas;
-			sliderObj.draw = drawFunc;
-
-		} else {
-			// VML fallback for IE 7 and 8
-
-			jsc.initVML();
-
-			var vmlContainer = document.createElement('div');
-			vmlContainer.style.position = 'relative';
-			vmlContainer.style.overflow = 'hidden';
-
-			var grad = document.createElement(jsc._vmlNS + ':fill');
-			grad.type = 'gradient';
-			grad.method = 'linear';
-			grad.angle = '180';
-
-			var rect = document.createElement(jsc._vmlNS + ':rect');
-			rect.style.position = 'absolute';
-			rect.style.left = -1 + 'px';
-			rect.style.top = -1 + 'px';
-			rect.stroked = false;
-			rect.appendChild(grad);
-			vmlContainer.appendChild(rect);
-
-			var drawFunc = function (width, height, color1, color2) {
-				vmlContainer.style.width = width + 'px';
-				vmlContainer.style.height = height + 'px';
-
-				rect.style.width = (width + 1) + 'px';
-				rect.style.height = (height + 1) + 'px';
-
-				grad.color = color1;
-				grad.color2 = color2;
-			};
-			
-			sliderObj.elm = vmlContainer;
-			sliderObj.draw = drawFunc;
-		}
+		sliderObj.elm = canvas;
+		sliderObj.draw = drawFunc;
 
 		return sliderObj;
 	},
@@ -1250,61 +1115,40 @@ var jsc = {
 			draw: null
 		};
 
-		if (jsc.isCanvasSupported) {
-			// Canvas implementation for modern browsers
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
 
-			var canvas = document.createElement('canvas');
-			var ctx = canvas.getContext('2d');
+		var drawFunc = function (width, height, color) {
+			canvas.width = width;
+			canvas.height = height;
 
-			var drawFunc = function (width, height, color) {
-				canvas.width = width;
-				canvas.height = height;
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
+			var sqSize = canvas.width / 2;
+			var sqColor1 = jsc.pub.chessboardColor1;
+			var sqColor2 = jsc.pub.chessboardColor2;
 
-				var sqSize = canvas.width / 2;
-				var sqColor1 = jsc.pub.chessboardColor1;
-				var sqColor2 = jsc.pub.chessboardColor2;
+			// dark gray background
+			ctx.fillStyle = sqColor1;
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-				// dark gray background
-				ctx.fillStyle = sqColor1;
-				ctx.fillRect(0, 0, canvas.width, canvas.height);
+			for (var y = 0; y < canvas.height; y += sqSize * 2) {
+				// light gray squares
+				ctx.fillStyle = sqColor2;
+				ctx.fillRect(0, y, sqSize, sqSize);
+				ctx.fillRect(sqSize, y + sqSize, sqSize, sqSize);
+			}
 
-				for (var y = 0; y < canvas.height; y += sqSize * 2) {
-					// light gray squares
-					ctx.fillStyle = sqColor2;
-					ctx.fillRect(0, y, sqSize, sqSize);
-					ctx.fillRect(sqSize, y + sqSize, sqSize, sqSize);
-				}
+			var grad = ctx.createLinearGradient(0, 0, 0, canvas.height);
+			grad.addColorStop(0, color);
+			grad.addColorStop(1, 'rgba(0,0,0,0)');
 
-				var grad = ctx.createLinearGradient(0, 0, 0, canvas.height);
-				grad.addColorStop(0, color);
-				grad.addColorStop(1, 'rgba(0,0,0,0)');
+			ctx.fillStyle = grad;
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+		};
 
-				ctx.fillStyle = grad;
-				ctx.fillRect(0, 0, canvas.width, canvas.height);
-			};
-
-			sliderObj.elm = canvas;
-			sliderObj.draw = drawFunc;
-
-		} else {
-			// NOT SUPPORTED
-			// VML fallback for IE 7 and 8
-
-			jsc.initVML();
-
-			var vmlContainer = document.createElement('div');
-			vmlContainer.style.position = 'relative';
-			vmlContainer.style.overflow = 'hidden';
-
-			var drawFunc = function (width, height, color1, color2) {
-				// NOT SUPPORTED
-			};
-			
-			sliderObj.elm = vmlContainer;
-			sliderObj.draw = drawFunc;
-		}
+		sliderObj.elm = canvas;
+		sliderObj.draw = drawFunc;
 
 		return sliderObj;
 	},
